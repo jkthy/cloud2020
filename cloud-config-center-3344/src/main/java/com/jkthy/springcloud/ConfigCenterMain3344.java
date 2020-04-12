42338d1ae9d9f094bf0aba5c46d6df75ee2ed1c6
江坤 <1115767374@qq.com>
2020-04-12 15:04:55 +0800

    config

---
+++ cloud-config-center-3344/src/main/java/com/jkthy/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.jkthy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author jk-thy
+ * @date 20/4/11 21:04
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/jkthy/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.jkthy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jk-thy
+ * @date 20/4/11 21:37
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
