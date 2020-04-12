03a25816c71594ea81e6b701cf1653278dd3e035
江坤 <1115767374@qq.com>
2020-04-12 15:37:03 +0800

    bus and rabbitmq

---
+++ cloud-config-client-3366/src/main/java/com/jkthy/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.jkthy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jk-thy
+ * @date 20/4/12 15:07
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/jkthy/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.jkthy.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jk-thy
+ * @date 20/4/12 15:09
+ */
+@RestController
+//实现刷新功能
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
